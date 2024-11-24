d1b26ab9e7b0688ca063353eaa56520a2c986b13
rhaneyko <rhaneykohonorio@gmail.com>
2024-11-24 12:28:26 -0400

    profile stylization

--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -1,7 +1,7 @@
 import { Header } from "../../components/Header/index";
 import background from "../../assets/background.png";
 
-import './styles.css'
+import "./styles.css";
 
 function App() {
   return (
@@ -11,9 +11,22 @@ function App() {
         <img src={background} className="background" alt="background app" />
         <div className="info">
           <div>
-            <input name="usuario" placeholder="@username"/>
+            <input name="usuario" placeholder="@username" />
             <button>Buscas</button>
           </div>
+          <div className="profile">
+            <img
+              src="https://avatars.githubusercontent.com/u/80545536?v=4"
+              className="profile-pic"
+              alt="profile-pic"
+            />
+            <div>
+              <h3>Rhaneyko Honorio</h3>
+              <span>@rhaneyko</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr/>
         </div>
       </div>
     </div>
